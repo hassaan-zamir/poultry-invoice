61afe1cd5ed3341f846f641fe6bc8a40ee4d5ffb
hassaan-zamir <hassaanzamir94@gmail.com>
2023-12-27 01:06:53 +0500

    changes

--- src/pages/advance-slip/[...id].tsx
+++ src/pages/advance-slip/[...id].tsx
@@ -4,25 +4,26 @@ import { useRouter } from "next/router";
 import { useEffect, useRef, useState } from "react";
 import { InvoiceType } from "..";
 import Head from "next/head";
-import { useReactToPrint } from 'react-to-print';
+import { useReactToPrint } from "react-to-print";
 
 export default function AdvanceSlip() {
   const router = useRouter();
   const componentRef = useRef(null);
 
-  const [invoice , setInvoice] = useState<InvoiceType | null>(null);
+  const [invoice, setInvoice] = useState<InvoiceType | null>(null);
 
   const handlePrint = useReactToPrint({
     content: () => componentRef.current,
   });
 
-  const getChallanNo = (d:string, id: number):string => {
+  const getChallanNo = (d: string, id: number): string => {
     const date = new Date(d);
-    return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}-${id}`;
-  }
+    return `${
+      date.getMonth() + 1
+    }/${date.getDate()}/${date.getFullYear()}-${id}`;
+  };
   const fetchInvoice = async (id: string | string[] | undefined) => {
-    if(!id)
-      return;
+    if (!id) return;
 
     try {
       const resp = await axios.post("/api/getInvoice", { id: id });
@@ -39,49 +40,59 @@ export default function AdvanceSlip() {
   if (!invoice) {
     return <p>Please wait...</p>;
   }
-  
 
-  return <>
-    <table  ref={componentRef} style={{ textAlign: 'center'}}>
-      <tr>
-        <th colSpan={4}>Golden Poulty Farms</th>
-      </tr>
-      <tr>
-        <td colSpan={4}>Contact: 0317825800 , 0317825900</td>
-      </tr>
-      <tr>
-        <th>Date</th>
-        <td>{new Date(invoice.date).toISOString().substr(0, 10)}</td>
-        <th>Challan#</th>
-        <td>{getChallanNo(invoice.date, invoice.id)}</td>
-      </tr>
+  return (
+    <>
+      <Head>
+        <link rel="stylesheet" href="/posPrint.css"/>
+      </Head>
+      <table
+        ref={componentRef}
+        style={{ textAlign: "center", background: "#fff", color: "#000", border:'none' }}
+        border={1} cellSpacing={10}
+      >
+        <tbody>
+          <tr>
+            <th colSpan={4}>Golden Poulty Farms</th>
+          </tr>
+          <tr>
+            <td colSpan={4}>Contact: 0317825800 , 0317825900</td>
+          </tr>
+          <tr>
+            <th>Date</th>
+            <td>{new Date(invoice.date).toISOString().substr(0, 10)}</td>
+            <th>Challan#</th>
+            <td>{getChallanNo(invoice.date, invoice.id)}</td>
+          </tr>
 
-      <tr>
-        <th>Shed</th>
-        <td>{invoice.shed}</td>
-        <th>Broker</th>
-        <td>{invoice.broker_name}</td>
-      </tr>
+          <tr>
+            <th>Shed</th>
+            <td>{invoice.shed}</td>
+            <th>Broker</th>
+            <td>{invoice.broker_name}</td>
+          </tr>
 
-      <tr>
-        <th>Vehicle</th>
-        <td>{invoice.vehicle_no}</td>
-        <th>Driver</th>
-        <td>{invoice.driver_name}</td>
-      </tr>
+          <tr>
+            <th>Vehicle</th>
+            <td>{invoice.vehicle_no}</td>
+            <th>Driver</th>
+            <td>{invoice.driver_name}</td>
+          </tr>
 
-      <tr>
-        <th>Cash</th>
-        <td>{invoice.cash}</td>
-        <th>Online</th>
-        <td>{invoice.online}</td>
-      </tr>
+          <tr>
+            <th>Cash</th>
+            <td>{invoice.cash}</td>
+            <th>Online</th>
+            <td>{invoice.online}</td>
+          </tr>
 
-      <tr>
-        <th colSpan={2}>Total Advance</th>
-        <td colSpan={2}>{invoice.cash + invoice.online}</td>
-      </tr>
-    </table>
-    <button onClick={handlePrint}>Print this out!</button>
-  </>
+          <tr>
+            <th colSpan={2}>Total Advance</th>
+            <td colSpan={2}>{invoice.cash + invoice.online}</td>
+          </tr>
+        </tbody>
+      </table>
+      <button onClick={handlePrint}>Print this out!</button>
+    </>
+  );
 }
